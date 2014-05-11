bc0d42ab9e570dca2b6612cc9b4a44504d275c05
TCA_TM <kben@xs4all.nl>
2014-05-11 11:21:51 +0200

    Updated client classes Added Client callback methods Added more user commands

--- plugins/TCAClient/src/com/koen/tca/client/ClientRMI.java
+++ plugins/TCAClient/src/com/koen/tca/client/ClientRMI.java
@@ -1,65 +1,178 @@
 package com.koen.tca.client;
+import java.rmi.NoSuchObjectException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
-public class ClientRMI {
+public class ClientRMI extends UnicastRemoteObject implements ICallBackClient {
 
-	// Data members
 
+	/**
+	 * serialVersionUID is needed because ClientRMI is a serializable class
+	 * The sender and receiver of a serialized object checks if this number is equeal to each other.
+	 * Otherwise an InvalidClassException is thrown.
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	private final String serverAddress = "192.168.56.1"; // The server- IP
 															// address
 	private final int serverPort = 1099; // The Port for the Client-Server
 											// communication. By default RMI use
 											// 1099
-	private final String serverRmi = "ServerRMI";
+	private final String serverRegistryName = "ServerRMI";
 
-	private RmiServerInterface serverObj;
+	private IRemoteServer remoteServer;
 	private Registry registry;
 
-	// Constructors
-	public ClientRMI() {
+	
+	public ClientRMI() throws RemoteException{
 
 		try {
 			// Get the Registry from the Server
 			registry = LocateRegistry.getRegistry(serverAddress, serverPort);
 
-			// Finds the remove Server object
-			serverObj = (RmiServerInterface) registry.lookup(serverRmi);
+			// Finds the remote Server object
+			remoteServer = (IRemoteServer) registry.lookup(serverRegistryName);
 
-		} catch (RemoteException e) {
-			System.out.println("Server is not online2!");
-		} catch (NotBoundException e) {
-			System.out
-					.println("the Server is not Registerd (bounded) int the registry!\n try to start te server first.\n");
+		} catch (RemoteException | NotBoundException  e) {
+			System.out.println("Server is not Registered (bounded) in the registry, or the Server is not online!");
 		}
+		
 	}
 
-	// Methods
-	public void startTest() {
+	public synchronized void startTestSet() {
 		try {
-			serverObj.StartTest();
+			remoteServer.startTestSet(this, "");
 		} catch (RemoteException e) {
-			System.out.println("Server is not online3!");
+			System.out.println("Server is not online!");
 		}
 
 	}
-
-	public void stopTest() {
+	
+	public synchronized void startTestCase (String testCase) {
 		try {
-			serverObj.StopTest();
+			remoteServer.StartTestCase(this, testCase);
+		} catch (RemoteException e) {
+			System.out.println ("Server is not online!");
+		}
+	}
+
+	public synchronized void stopTest() {
+		try {
+			remoteServer.stopTest(this);
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void addTestCase(String testCaseName) {
+	public synchronized void uploadTestScript(String testCaseName) {
 		try {
-			serverObj.AddTestCase(testCaseName);
+			remoteServer.uploadTestCase(testCaseName, null);
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public synchronized void startDetect () {
+		try {
+			remoteServer.startDetect(this);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public synchronized void stopDetect () {
+		try {
+			remoteServer.StopDetect(this);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public synchronized TestResults getTestResults () {
+		TestResults testResults = null;
+		try {
+			testResults = remoteServer.getTestResults();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		return testResults;
+	}
+	public synchronized void getServerStatus () {
+		try {
+			remoteServer.getServerStatus();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public synchronized String [] getUEList () {
+		String [] ueList = null;
+		try {
+			ueList = remoteServer.getUEList();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		return ueList;
+	}
+	
+	@Override
+	public void testIsDone() throws RemoteException {
+
+		System.out.println("\nThe test is Finished!\n\n");
+		System.out.print("TCA>");		
+		try {
+			unexportObject (this, true);
+		} catch (NoSuchObjectException e) {
+			// The RMI runtime thinks we're not a server, so thats fine.
+		}
+		
+	}
+
+	@Override
+	public void testFailed(String reason) throws RemoteException {
+		System.out.println("\nThe test has failed!\n");
+		System.out.println("reason:" + reason + "\n\n");
+		System.out.print("TCA>");		
+		try {
+			unexportObject (this, true);
+		} catch (NoSuchObjectException e) {
+			// The RMI runtime thinks we're not a server, so thats fine.
+		}
+		
+	}
+
+	@Override
+	public void detectFailed(String reason) throws RemoteException {
+		System.out.println("\nDetecting for user equipments has failed!\n");
+		System.out.println("reason:" + reason + "\n\n");
+		System.out.print("TCA>");		
+		try {
+			unexportObject (this, true);
+		} catch (NoSuchObjectException e) {
+			// The RMI runtime thinks we're not a server, so thats fine.
+		}
+	
+		
+	}
+
+	@Override
+	public void detectIsDone(String[] uelist) throws RemoteException {
+		System.out.println("\nDetecting for user equipments is finished!\n");
+		System.out.println("User equipments found:\n");
+		for (String ue : uelist) {
+			System.out.println ("\t" + ue + "\n");
+		}
+		System.out.print("\nTCA>");		
+
+		try {
+			unexportObject (this, true);
+		} catch (NoSuchObjectException e) {
+			// The RMI runtime thinks we're not a server, so thats fine.
+		}
+		
 	}
 
 }
--- plugins/TCAClient/src/com/koen/tca/client/TCAClient.java
+++ plugins/TCAClient/src/com/koen/tca/client/TCAClient.java
@@ -1,70 +1,150 @@
+
 package com.koen.tca.client;
+
+import java.io.*;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+
 public class TCAClient {
 
 	public static void main(String[] args) {
 
 		// the messages(string) that shows the possible parameters on the
 		// command line
-		final String mainParameters = "Possible parameters are:\n   Start\n   Stop\n   Addscript <scriptfile>\n";
+		final String mainParameters = "Possible parameters are:\n"
+				+ "   starttestset\n"
+				+ "   starttestcase <testcase name>\n" 
+				+ "   stoptest\n"
+				+ "   startdetect\n"
+				+ "   stopdetect\n"
+				+ "   uploadscript <scriptfile>\n"
+				+ "   gettestresults\n"
+				+ "   serverstatus\n"
+				+ "   getuelist\n"
+				+ "   ?\n"
+				+ "   exit\n";
 
 		final String welcomeText = "TCA version 1.0 \na Communication application to testcase Server.\n\n ";
-
-		final int FIRST_ARG = 0; // The index for the first argument that is
-									// given by the command line
-		final int SECOND_ARG = 1; // the index for the second argument that is
-									// given by the command line
-
+		final String exitText = "Thank you and goodby!";
+		
+		BufferedReader commandlineText = new BufferedReader(new InputStreamReader(System.in));		
+		
+		
 		// Show welcome display on the command line
 		System.out.println(welcomeText);
 
 		// Initiate a new ClinetRMI object witch communicate with the Server
-		ClientRMI client = new ClientRMI();
+		ClientRMI client;
+		try {
+			client = new ClientRMI();
 
-		// Check for parameters on the command line
-		if (args.length > 0) {
-			// Switch to the right method dependent on the first argument from
-			// the command line
 
-			switch (args[FIRST_ARG].toLowerCase()) {
-			case "start":
-				System.out.println("Starting the test...\n");
-				client.startTest();
-				System.out.println("done.");
-				break;
+		String command = "";
+		String param = "";
 
-			case "stop":
-				System.out.println("Stopping the test..\n");
-				client.stopTest();
-				System.out.println("done.");
-				break;
+		int index = 0;
+		String tmp = "";
+		while (!command.matches("exit")) {
+			System.out.print("\nTCA>");
 
-			case "addscript":
-				System.out.println("Adding a testcase...\n");
-
-				// Check if the second command line parameter (the script file
-				// name) exists.
-				if (args.length > 1) {
-					client.addTestCase(args[SECOND_ARG]);
-					System.out.println("done.");
+			try {
+				tmp = commandlineText.readLine();
+				tmp.toLowerCase();
+			if ((index = tmp.indexOf(" ")) >= 0) {
+					param = tmp.substring(index+1);	// gets the substring after the command string
+					command = tmp.substring(0, index);
 				} else {
-					System.out
-							.println("No testcase filename!\nAdding a testcase is stopped!\n");
-					System.out.println(mainParameters);
+					command = tmp.substring(0);
 				}
-				break;
-			default:
-				System.out.println("No valid paramers!\n");
-				System.out.println(mainParameters);
-				break;
-			}
 
-		} else {
-			// No parameters on the command line. Show the possible parameters
-			// on the command line
-			System.out.println("No parameters present!");
-			System.out.println(mainParameters);
+				switch (command) {
+				case "starttestset":
+					System.out.println("Starting the test...\n");
+					client.startTestSet();
+					break;
+
+				case "starttestcase":
+					System.out.println("Starting the test...\n");
+					client.startTestCase(param);
+					break;
+				
+				
+				case "stoptest":
+					System.out.println("Stopping the test..\n");
+					client.stopTest();
+					System.out.println("done.");
+					break;
+
+				case "Uploadscript":
+					System.out.println("Uploading testcase " + param + "\n");
+
+					// Check if the second command line parameter (the script file
+					// name) exists.
+					if (args.length > 1) {
+						client.uploadTestScript(param);
+						System.out.println("done.");
+					} else {
+						System.out.println("No testcase filename!\n");
+						System.out.println(mainParameters);
+					}
+					break;
+					
+				case "startdetect":
+					System.out.println("Detecting for UE's is starting...\n");
+					client.startDetect();
+					break;
+					
+				case "stopdetect":
+					System.out.println("Detecting for UE's is stopping...\n");
+					client.stopDetect();
+					break;
+					
+				case "gettestresults":
+					System.out.println("Getting test results...\n");
+					client.getTestResults();
+					break;
+					
+				case "serverstatus":
+					System.out.println("Status of the Server:\n");
+					client.getServerStatus();
+					break;
+				
+				case "getuelist":
+					String [] uelist = null;
+					System.out.println("Getting known user equipments...\n");
+					uelist = client.getUEList();
+					for (String ue : uelist) {
+						System.out.println ("\t" + ue + "\n");
+					}
+					break;
+					
+				case "?":
+					System.out.println(mainParameters);
+					break;
+				case "exit":
+					System.out.println ("HOI:" + command + ":");
+					break;
+					
+				default:
+					System.out.println("No valid command\n");
+
+				break;
+				}
+				
+			} catch (Exception e) {
+				
+			}
+			
 		}
 
-	}
+		// Shows the last exit text.
+		System.out.println (exitText);
 
-}
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+
+	}  // main
+
+	
+} // class TCAClient
