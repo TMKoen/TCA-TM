913dea69d5bcfd01a8aa74bb6595f72d591a61d3
Christophe Bouhier <dzonekl@gmail.com>
2014-06-20 12:06:04 +0200

    - Server product definition. Includes OSGI console. (telnet [host] 8811) Includes equinox application. TCAServer is launched through the TCAServer Activator. (Not the Application)

---
+++ plugins/TCAServer/src/com/koen/tca/server/EquinoxApplication.java
@@ -0,0 +1,18 @@
+package com.koen.tca.server;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class EquinoxApplication implements IApplication {
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		return null;
+	}
+
+	@Override
+	public void stop() {
+
+	}
+
+}
